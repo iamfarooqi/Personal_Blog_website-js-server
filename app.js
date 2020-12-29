9fc0895ae24c5f9487eab6993c61eda5b6e2adb3
iamfarooqi <attaurrahmanfarooqi9@gmail.com>
2020-12-29 17:33:49 +0500

    server not done

--- app.js
+++ app.js
@@ -1,46 +1,61 @@
 // Login
 
-var user = ["farooqi@gmail.com", "12345"]
+// var user = ["farooqi@gmail.com", "12345"]
 
 //    var email = "farooqi@gmail.com" ;
 //    var password = "12345";
+var url = "http://localhost:5000";
 
 function login() {
 
     email = document.getElementById("email").value;
     password = document.getElementById("password").value;
 
-    if (email === "farooqi@gmail.com" && password === "12345"){
 
-         alert("success")
 
-         window.location.href = "./index.html";
-    }else{
-        alert("wrong")
+    const Http = new XMLHttpRequest();
+
+    Http.open("POST", url + "/index");
+    Http.setRequestHeader("Content-Type", "application/json");
+    Http.send(JSON.stringify({
+        email : email,
+        password : password
+    }));
+    Http.onreadystatechange = (e) => {
+        alert(Http.responseText)
     }
-    
-
-
     return false;
 }
 
-
 //Dashboard
 
-var newpost = [];
+// var newpost = [];
+// var title;
 
 function recentpost() {
 
-    newpost.push(document.getElementById("post").value);
+    // title = document.getElementById("title").value;
 
-    document.getElementById("heading").innerHTML = "Recent Blog";
-    document.getElementById("recent-post").innerHTML = newpost;
+    newpost.push({
+        title: document.getElementById("title").value,
+        post: document.getElementById("post").value
+    });
 
+    for (var i = 0; i < newpost.length; i++) {
+        document.getElementById("heading").innerHTML = newpost[i].title;
+        document.getElementById("recent-post").innerHTML = newpost[i].post;
+    }
 
+    const Http = new XMLHttpRequest();
 
+    Http.open("POST", url + "/index");
+    Http.setRequestHeader("Content-Type", "application/json");
+    Http.send(JSON.stringify(newpost));
+    Http.onreadystatechange = (e) => {
+        //  JSON.parse(Http.responseText);
+        console.log(Http.responseText)
+        // alert(JsonResp.message);
 
-
-
-
-    return false;
+    }
+    return false
 }
--- server.js
+++ server.js
@@ -1,15 +1,32 @@
+var admin = {
+    email: "farooqi@gmail.com",
+    pass: "12345"
+}
+var PORT = process.env.PORT || 5000;
+var express = require("express");
+var cors = require("cors");
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+let users = [];
+const app = express();
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.get("/", (req, res, next) => {
+    res.send("Hello")
+})
+
+app.post("/index", (req, res, next) => {
+    if (admin.email === req.body.email && admin.pass === req.body.password) {
+        res.send("Login")
+    } else {
+        res.send("wrong")
+    }
+})
 
 
-var express = require("express")
-var app = express();
-
-app.get("/login", function(req, res) {
-   res
-   .status(200)
-   .send("we are on login page")
-});
-
-
-app.listen(3000, function(){
-    console.log("we are serving at ")
-});
+app.listen(PORT, () => {
+    console.log("server is running on " + PORT);
+})
